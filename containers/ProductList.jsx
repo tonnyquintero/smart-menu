63129887b6e231d92e022b133e53e79ed9312973
tonny187 <tonyoanthony@gmail.com>
2022-05-31 19:31:02 -0500

    agregando arrays de secciones

--- containers/ProductList.jsx
+++ containers/ProductList.jsx
@@ -1,38 +1,67 @@
-import React from "react";
-import ProductItem from "../components/ProductItem.jsx";
-import useGetProducts from "../Hooks/useGetProducts";
-import styles from "../styles/ProductList.module.css";
-import data from "../pages/api/data.js";
+import React from 'react';
+import ProductItem from '../components/ProductItem.jsx';
+import useGetProducts from '../Hooks/useGetProducts';
+import styles from '../styles/ProductList.module.css';
+import data from '../pages/api/data.js';
 
 const API = data;
 
 const ProductList = () => {
-  const products = useGetProducts(API);
-  return (
-    <section className={styles["main-container"]}>
-      <div className={styles.ProductList}>
-        {products.map((product) => {
-          if (product.images.length > 0 && product.images[0] !== "") {
-            return <ProductItem product={product} key={product.id} />;
-          }
-        })}
-      </div>
-      <div className={styles.ProductList}>
-        {products.map((product) => {
-          if (product.images.length > 0 && product.images[0] !== "") {
-            return <ProductItem product={product} key={product.id} />;
-          }
-        })}
-      </div>
-      <div className={styles.ProductList}>
-        {products.map((product) => {
-          if (product.images.length > 0 && product.images[0] !== "") {
-            return <ProductItem product={product} key={product.id} />;
-          }
-        })}
-      </div>
-    </section>
-  );
+    const products = useGetProducts(API);
+    return (
+        <section className={styles['main-container']}>
+            <h2>RECOMENDADOS</h2>
+            <div className={styles.ProductList}>
+                {products
+                    .filter((product) => product.recomended === true)
+                    .map((filtrado) => {
+                        if (filtrado.images.length > 0 && filtrado.images[0] !== '') {
+                            return <ProductItem product={filtrado} key={filtrado.id} />;
+                        }
+                    })}
+            </div>
+            <h2>LASAGNAS</h2>
+            <div className={styles.ProductList}>
+                {products
+                    .filter((product) => product.type === 'Lasagnas')
+                    .map((filtrado) => {
+                        if (filtrado.images.length > 0 && filtrado.images[0] !== '') {
+                            return <ProductItem product={filtrado} key={filtrado.id} />;
+                        }
+                    })}
+            </div>
+            <h2>PASTAS</h2>
+            <div className={styles.ProductList}>
+                {products
+                    .filter((product) => product.type === 'Pastas')
+                    .map((filtrado) => {
+                        if (filtrado.images.length > 0 && filtrado.images[0] !== '') {
+                            return <ProductItem product={filtrado} key={filtrado.id} />;
+                        }
+                    })}
+            </div>
+            <h2>HAMBURGUESAS</h2>
+            <div className={styles.ProductList}>
+                {products
+                    .filter((product) => product.type === 'Hamburguesas')
+                    .map((filtrado) => {
+                        if (filtrado.images.length > 0 && filtrado.images[0] !== '') {
+                            return <ProductItem product={filtrado} key={filtrado.id} />;
+                        }
+                    })}
+            </div>
+            <h2>BEBIDAS</h2>
+            <div className={styles.ProductList}>
+                {products
+                    .filter((product) => product.type === 'Bebidas')
+                    .map((filtrado) => {
+                        if (filtrado.images.length > 0 && filtrado.images[0] !== '') {
+                            return <ProductItem product={filtrado} key={filtrado.id} />;
+                        }
+                    })}
+            </div>
+        </section>
+    );
 };
 
 export default ProductList;
